4019b1cfaba8f3882d8bb2f5ba4c6120bc67d37d
Andrei Vintila <vintila.andrei@gmail.com>
2022-12-14 21:40:42 +0200

    3

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <iframe
+    width="560"
+    height="315"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometor; autoplay; clipboard-write; encrypted-media; gyroscope"
+  ></iframe>
+</template>
